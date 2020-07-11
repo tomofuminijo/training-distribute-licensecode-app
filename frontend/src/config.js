cb8d736c5dc32450b2068f87a7429d7e5240a512
nijt <nijot@leanhorus.com>
2020-07-11 13:51:11 +0000

    Update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,8 +2,7 @@ export var amplify_config = {
     API: {
         endpoints: [{
             name: 'LicenseCodeDistributorAPI',
-            endpoint: 'https://b49f7n0wjh.execute-api.ap-northeast-1.amazonaws.com/prod', // API Gateway Endpoint. such as https://abcdefghij.execute-api.us-west-2.amazonaws.com/prod
-            region: 'ap-northeast-1' // Amazon service region
+            endpoint: 'https://2jxjzo3910.execute-api.ap-northeast-1.amazonaws.com/prod' // API Gateway Endpoint. such as https://abcdefghij.execute-api.us-west-2.amazonaws.com/prod
         }]
     }
 };
